3bd23255fb94b4a9ac6f2f2673d6f5077f30e4be
salmanahmedxyz <salmanahmedx@hotmail.com>
2022-02-28 12:59:37 +0600

    dynamic api

--- api.js
+++ api.js
@@ -18,3 +18,22 @@ const showComment = (data) => {
 }
 
 // dynamic api JSON placeholder
+const dynamicBtn = document.querySelector(".dynamic-comment")
+const numberInput = document.querySelector(".number")
+
+dynamicBtn.addEventListener('click', function () {
+    const newInputValue = Number(numberInput.value);
+    dynamicApi(newInputValue)
+
+})
+
+const dynamicApi = (postId) => fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+    .then(response => response.json())
+    .then(data => showData(data))
+
+function showData(data) {
+    const p = document.querySelector(".dynamic-comments")
+    const firstObject = data[0];
+    p.textContent = firstObject.body;
+}
+
